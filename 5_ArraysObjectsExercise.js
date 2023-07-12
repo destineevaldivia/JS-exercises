7242d69f90d16f2870a78b5d113b36fffd24ede8
destineevaldivia <destvaldivia@gmail.com>
2023-07-12 15:04:18 -0700

    completed exercise #5

--- 5_ArraysObjectsExercise.js
+++ 5_ArraysObjectsExercise.js
@@ -46,7 +46,7 @@ const listOfAssortedThings= " ";
 for (let i = 0; i < assortedThings.length; i++) {
     console.log("Item #"+ i + " is " + assortedThings[i]);
 }
-return listOfAssortedThings;
+console.log(listOfAssortedThings);
 
 // Exercise 11. Write a function that takes an array of numbers as a parameter. For each number in the array,
 // print the number and "BIG" if it's over 100, "small" if it's between 0 and 100, and "negative" if
@@ -58,20 +58,62 @@ return listOfAssortedThings;
 //
 // Then write some function calls you would use to test your function.
 
+function printNumSizes (arrOfNums) {
+    let arrNumSizes = " ";
+
+    for (let i = 0; i < arrOfNums.length; i++) {
+        if (arrOfNums[i] > 100) {
+            console.log(`${arrOfNums[i]} BIG`);
+        }
+        else if (arrOfNums[i] < 0) {
+            console.log(`${arrOfNums[i]} negative`);
+        }
+        else if (arrOfNums[i] < 100) {
+            console.log(`${arrOfNums[i]} small`);
+        }
+    } 
+}
+printNumSizes([5, 400, -6]);
+printNumSizes([-5, -400, -7]);
+printNumSizes([2000, 200, 222]);
+
+
+ 
 // Exercise 12. Make an object called me that contains the following keys: name, favoriteAnimal, favoriteNumber
 // The values of those keys can be whatever you choose.
+const me = {
+    name: "Tim",
+    favoriteAnimal: "turtles",
+    favoriteNumber: 2
+};
 
 // Exercise 13. Add another 2 keys and values to the object (you choose what they are)
+me.favoriteFood = "french fries";
+me.bestfriend = "Fred";
 
 // Exercise 14. Update the favoriteAnimal value to something different
+me.favoriteAnimal = "frogs";
 
 // Exercise 15. Print the value of favoriteAnimal.
 // Note: there are at least 2 ways to get the value of a key, try to write both.
 
+//accessing value with dot notation
+const favoriteAnimalValueDotNotation = me.favoriteAnimal;
+    console.log(favoriteAnimalValueDotNotation);
+
+//accessing value with bracket notation
+const favoriteAnimalValueBracketNotation = me["favoriteAnimal"];
+    console.log(favoriteAnimalValueBracketNotation);
+
+
+
 // Congrats, you made it to the end!
 // Did you find this easy or hard? If you used references, which ones helped you?
 // Please answer in a comment below.
-//
+    //I found that most of the problems were easy, except for #11 was a bit more challenging and took more time to figure out syntax errors. 
+    //Also, in the previous problem #10 I used a return statement, which made my code "not reachable". 
+    //I understand why now (vs code interpretted the whole file as a function, for good reason and didn't run the rest of my code)
+    // to resolve, I switched used console.log on line 49 instead and this fixed my errors. 
 
 // Email your file to the course staff,
 // or commit your file to GitHub and email us a link.
